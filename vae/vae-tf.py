7c232c8f45d983d48dd3d824f21f22b15b793a49
Stephen Lee <stephenl7797@gmail.com>
2018-04-05 12:44:25 -0400

    vae loss

--- vae/vae-tf.py
+++ vae/vae-tf.py
@@ -1,34 +1,65 @@
 import tensorflow as tf
 import numpy as np
-import sys
+import sys,random
 import matplotlib.pyplot as plt
 
 # Data
-from tensorflow.examples.tutorials.mnist import input_data
+#from tensorflow.examples.tutorials.mnist import input_data
 
-IM_SIZE = [28,28,1]
-lr = .003
+CHAR_SIZE = 56
+#IM_SIZE = [28,28,1]
+IM_SIZE = [CHAR_SIZE,CHAR_SIZE,1]
+lr = .0003
 CLASSES = 10
 BATCH_SZ = 24
 LATENT_DIMS = 49
 
-'''
 import matplotlib.font_manager as mfm
 
-font_path = "/data/SourceHansSerif-Regular.otf"
+font_path = "/data/SourceHanSerif-Regular.otf"
 prop = mfm.FontProperties(fname=font_path)
-#plt.text(0.5, 0.5, s=u'测试', fontproperties=prop)
 
-def gen_char_gen(n=50)
-    #plt.text(0.5, 0.5, s=u'测试', fontproperties=prop)
+# Returns a batch sampler function
+def gen_char_gen(n=50, size=CHAR_SIZE):
+
     start = 20272
-    chars = ['\u'+hex(q) for q in range(start,start+n)]
-    ims = 
+    chars = [str(chr(q)) for q in range(start,start+n)]
 
-    def gene():
-        random.sample(chars)
-'''
+    ims = np.zeros([n,size,size,1])
+    inset = .1
+    #font_range = (9,13+1)
+    #font_range = (2,3)
+    font_range = (120,200)
+    char_w = (.2,.3)
 
+    fig = plt.figure(figsize=(4,4), dpi=size/4)
+
+    for i in range(n):
+        s = np.random.randint(*font_range)
+        zz = s/font_range[1]*char_w[1] + (1-s/font_range[1])*char_w[0]
+        x = np.random.uniform(0,1-zz)
+        y = np.random.uniform(0,1-zz)
+        plt.text(x,y,chars[i], fontsize=s, fontproperties=prop)
+        fig.canvas.draw()
+        #print(np.array(fig.canvas.renderer._renderer).shape)
+        im = np.array(fig.canvas.renderer._renderer)[...,0:1]
+        ims[i] = (im.max() - im) / im.max()
+        fig.clear()
+
+    plt.close(fig)
+    del fig
+
+    def gene(batch_size):
+        inds = np.random.choice((range(n)), batch_size)
+        x = ims[inds]
+        #f,a=plt.subplots(figsize=(5,5))
+        #a.imshow(x[0,...,0])
+        #plt.show()
+        return x,inds
+
+    return gene
+
+# --------------------------------------------------------------------------
 
 def mk_ae_net():
     x = tf.placeholder(tf.float32, shape=[None,*IM_SIZE])
@@ -46,9 +77,14 @@ def mk_ae_net():
 
     # Reparameterization:
     #   z = m + s*epsilon
-    mu  = tf.layers.dense(net, LATENT_DIMS)
-    sig = tf.layers.dense(net, LATENT_DIMS)
-    z = mu + sig * tf.random_normal(shape=[LATENT_DIMS])
+    mu  = tf.layers.dense(net, LATENT_DIMS, name='mu_op', use_bias=False)
+    sig = tf.layers.dense(net, LATENT_DIMS, name='sig_op', use_bias=False, activation=tf.nn.relu)
+    sig += tf.ones(LATENT_DIMS) * .1
+    mu  = tf.identity(mu, name='mu')
+    sig = tf.identity(sig, name='sig')
+    eps = tf.random_normal(shape=[LATENT_DIMS], name='eps')
+    z = tf.add(mu, sig * eps, name='z_op')
+    z = tf.identity(z,name='z')
 
     # Deconv (decoder)
     net = tf.layers.dense(z, sz)
@@ -63,8 +99,27 @@ def mk_loss(xh, xt):
     # log p = E[ logp(x,z) - logq(z|x) ]   -   KL[q(z|x) | p(z|x)] 
     #    ELBO __________________________
 
-    loss = tf.losses.mean_squared_error(xh, xt)
-    #opt_op = tf.train.GradientDescentOptimizer(lr).minimize(loss)
+    z = tf.get_default_graph().get_tensor_by_name('z:0')
+    eps = tf.get_default_graph().get_tensor_by_name('eps:0')
+    sig = tf.get_default_graph().get_tensor_by_name('sig:0')
+
+    d = LATENT_DIMS
+
+    # z ~ N(mu,sig)
+    log_pz = -1/(2*np.pi) - d - tf.reduce_sum(tf.square(eps))/2
+    log_qzx = log_pz - tf.reduce_sum(tf.log(sig))
+
+    # Recons score
+    #log_pzx = tf.losses.sigmoid_cross_entropy(xt>.5,xh)
+    recons_loss  = tf.losses.mean_squared_error(xt,xh)
+
+    # Negative loss because we are minizming w/ sgd (want to maxim)
+    loss0 = tf.reduce_sum(recons_loss)
+    loss1 = tf.reduce_mean(log_qzx) * .0000001
+    loss = loss0 + loss1
+
+    loss = tf.Print(loss,[loss0,loss1])
+
     opt_op = tf.train.AdamOptimizer(lr).minimize(loss)
     ret = tf.tuple([loss], control_inputs=[opt_op])
     return ret
@@ -73,17 +128,20 @@ def run(sess=None):
     print(" - Start.")
     sess = sess if sess else tf.InteractiveSession()
 
-    mnist = input_data.read_data_sets("/data/mnist/", one_hot=True)
+    #mnist = input_data.read_data_sets("/data/mnist/", one_hot=True)
 
     x_in,z,net = mk_ae_net()
     loss_train = mk_loss(net, x_in)
 
     sess.run(tf.global_variables_initializer())
 
+    char_gen = gen_char_gen(10)
+
     # loop
-    for batch in range(1200):
-        bx,by = mnist.train.next_batch(BATCH_SZ)
-        bx = bx.reshape([BATCH_SZ, 28,28, 1])
+    for batch in range(660):
+        #bx,by = mnist.train.next_batch(BATCH_SZ)
+        #bx = bx.reshape([BATCH_SZ, 28,28, 1])
+        bx,by = char_gen(BATCH_SZ)
 
         loss,xh = sess.run([loss_train,net], feed_dict={x_in:bx})
         loss = loss[0]
@@ -103,16 +161,23 @@ def run(sess=None):
     print(" - Done.")
 
     N = BATCH_SZ
-    a,b = bx[0],bx[1]
-    path = np.linspace(0,1,N)
-    cs = [path[i]*a + (1-path[i])*b for i in range(N)]
+    ps = []
+    for j in range(N-1):
+        a,b = bx[j],bx[j+1]
+        path = np.linspace(0,1,N)
+        cs = [path[i]*a + (1-path[i])*b for i in range(N)]
 
-    xhs = list(sess.run([net], feed_dict={x_in:cs})[0][...,0])
+        xhs = list(sess.run([net], feed_dict={x_in:cs})[0][...,0])
 
-    p = (np.hstack(xhs))
+        p = (np.hstack(xhs))
+        p += np.min(p)
+        p /= np.max(p)
+        ps.append(p)
+
+    pp = np.vstack(ps)
     plt.figure(figsize=(11,6))
-    plt.imshow(p,cmap='gray')
-    plt.imsave('interp.png',p,cmap='gray')
+    plt.imshow(pp,cmap='gray')
+    plt.imsave('interp.png',pp,cmap='gray')
     plt.show()
 
 
